c2ef344ebb48ef3c72a9e3557141d67afbd57c3c
helauren <helauren42@gmail.com>
2025-03-05 23:58:34 +0100

    counting syllables added

--- dictionnary/buildDb.cpp
+++ dictionnary/buildDb.cpp
@@ -1,4 +1,5 @@
 #include "buildDb.hpp"
+// #include <cppconn/driver.h>
 
 #define FILE "files/en_US.txt"
 
@@ -7,16 +8,18 @@ int main(int ac, char **av) {
     (void)av;
     // setlocale(LC_ALL, "");
     std::locale::global(std::locale("en_US.UTF-8"));
-    Logger::setLogger("logger/logger.log", Logger::DEBUG, true);
+    Logger::setLogger("logger/logger.log", Logger::INFO, true);
     std::wifstream readFile(FILE);
     if(!readFile) {
         stdErr("Error reading file");
         Logger::debug("Error reading file");
+        return 1;
     }
     std::wstring line;
+    std::list<Token> tokens;
     while(getline(readFile, line)) {
-        // stdOut("line: ", line);
         Token token = Token(line);
+        tokens.push_back(token);
     }
     return 0;
 }
--- dictionnary/buildDb.hpp
+++ dictionnary/buildDb.hpp
@@ -19,9 +19,12 @@ enum Type{
 
 namespace ipapronunciation
 {
-    static constexpr std::array<const wchar_t, 23> consonantFirstChars = {L'b', L'd', L'f', L'g', L'h', L'k', L'l', L'm', L'n', L'ŋ', L'p', L'r', L's', L'ʃ', L't', L'θ', L'ð', L'v', L'w', L'ʍ', L'j', L'z', L'ʒ'};
-    static constexpr std::array<const wchar_t, 13> vowelFirstChars = {L'æ', L'e', L'ɑ', L'ɛ', L'ɔ', L'i', L'ə', L'ɜ', L'a', L'ɒ', L'o', L'u', L'ʊ'};
-
+    static constexpr std::array<const wchar_t, 24> consonantFirstChars = {L'b', L'd', L'f', L'g', L'h', L'k', L'l', L'm', L'n', L'ŋ', L'p', L'r', L's', L'ʃ', L't', L'θ', L'ð', L'v', L'w', L'ʍ', L'j', L'z', L'ʒ', L'ɫ'};
+    static constexpr std::array<const wchar_t, 15> vowelFirstChars = {L'æ', L'e', L'ɑ', L'ɛ', L'ɔ', L'i', L'ə', L'ɜ', L'a', L'ɒ', L'o', L'u', L'ʊ', L'ɪ', L'ɝ'};
+    // static constexpr std::array<const wchar_t[3], 8> diphthongs = {
+    //     L"aɪ", L"eɪ", L"oʊ", L"aʊ", L"ɔɪ", L"ɪə", L"eə", L"ʊə"
+    // };
+    
     static bool isConsonant(const wchar_t c) {
         for (auto &cons : consonantFirstChars) {
             if (c == cons) {
@@ -53,38 +56,49 @@ private:
         int i = len -1;
         const Type type = findType(my_string[i]);
         if(type == CONSONANT)
-            while(i > 1 && ipapronunciation::isConsonant(my_string[i]))
+            while(i >= 1 && ipapronunciation::isConsonant(my_string[i -1]))
                 i--;
         else
-            while(i > 1 && ipapronunciation::isVowel(my_string[i]))
+            while(i >= 1 && ipapronunciation::isVowel(my_string[i -1]))
                 i--;
         return i;
     }
     void fetchIpaPronunciations(const std::vector<std::wstring> &strings_pronunciation)
     {
         for (std::wstring pronun : strings_pronunciation) {
-            // std::wcout << "str : " << str_pronunciation << std::endl;
             std::vector<std::wstring> phoneme_list;
             while(pronun.size()) {
-                // Logger::wdebug("pronun: ", pronun);
+                Logger::wdebug("pronun: ", pronun);
                 size_t start = findPhonemeStart(pronun);
-                // Logger::wdebug("start: ", start);
+                Logger::wdebug("start: ", start);
                 std::wstring phoneme = pronun.substr(start);
-                // Logger::wdebug("phoneme: \'", phoneme, "\'");
+                Logger::wdebug("phoneme: \'", phoneme, "\'");
                 if(phoneme.size() == 0)
                     continue;
                 phoneme_list.push_back(phoneme);
-                // Logger::wdebug("phoneme list: ", phoneme_list);
                 pronun = pronun.substr(0, start);
             }
             rev_ipa_pronunciations.push_back(phoneme_list);
             Logger::winfo("rev ipa: ", rev_ipa_pronunciations);
         }
     }
+    void setSyllables() {
+        for(auto& pronun : rev_ipa_pronunciations) {
+            unsigned int count = 0;
+            for(auto& phenome: pronun) {
+                if(ipapronunciation::isVowel(phenome[0])) {
+                    Logger::winfo("is vowel: ", phenome);
+                    count++;
+                }
+            }
+            syllables.push_back(count);
+        }
+    }
 
 public:
     std::wstring word;
     std::vector<std::vector<std::wstring>> rev_ipa_pronunciations;
+    std::vector<unsigned int> syllables;
     Token(std::wstring &line)
     {
         Logger::wdebug("before: ", line);
@@ -93,7 +107,10 @@ public:
         std::vector<std::wstring> line_split = wsplit<std::vector>(line, L" \t\n\r\v\f\u00A0\u200B");
         word = line_split[0];
         const std::vector<std::wstring> strings_pronunciation(line_split.begin() + 1, line_split.end());
+        Logger::winfo("word:", word);
         fetchIpaPronunciations(strings_pronunciation);
+        setSyllables();
+        Logger::winfo("Syllables: ", syllables);
     };
     ~Token() {};
 };
