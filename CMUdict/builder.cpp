23d3a25e9e228eae02865c595200c9980035c4a3
helauren <helauren42@gmail.com>
2025-03-18 13:10:33 +0100

    parsing into tokens correctly manually checked not better way, word, phonemes and syllable count correct

--- CMUdict/builder.cpp
+++ CMUdict/builder.cpp
@@ -4,10 +4,20 @@
 
 #define READFILE "files/cmudict-0.7b"
 
+unsigned int count_syllables(const vector<string> &phonemes) {
+  unsigned int count = 0;
+  for (auto &phoneme : phonemes) {
+    if (TokenMaker::isVowel(phoneme)) {
+      count++;
+    }
+  }
+  return count;
+};
+
 vector<string> readLines() {
   ifstream readStream(READFILE);
   if (!readStream.is_open()) {
-    WPrinter::stdErr("could not read file: ", READFILE);
+    Printer::stdErr("could not read file: ", READFILE);
   }
   std::string buffer;
   vector<std::string> lines;
@@ -21,13 +31,23 @@ vector<string> readLines() {
     }
     lines.push_back(line);
   };
-  for (auto line : lines) {
-    cout << line << endl;
-  }
+  // for (auto line : lines) {
+  //   cout << line << endl;
+  // }
   return lines;
 }
 
 int main() {
+  vector<Token> tokens;
   const vector<string> lines = readLines();
+  for (auto &line : lines) {
+    auto split_line = split<vector>(line, WHITE_SPACES);
+    Printer::stdOut(split_line);
+    if (split_line.empty() || split_line.size() < 2)
+      continue;
+    const Token &token = TokenMaker::makeToken(split_line);
+    tokens.push_back(token);
+  }
+  Printer::stdOut(tokens);
   return 0;
 }
--- CMUdict/builder.hpp
+++ CMUdict/builder.hpp
@@ -2,47 +2,68 @@
 
 #include <array>
 #include <iostream>
+#include <ostream>
+#include <string>
 #include <vector>
 
-#define FILE "files/cmudict-0.7b"
-
 using namespace std;
+using namespace Printer;
 
 class Token {
-private:
+public:
   const std::string word;
   const vector<string> phonemes;
   const unsigned int syllables;
 
-public:
   Token(const string &_word, const vector<string> &_phonemes,
         const int &_syllables)
       : word(_word), phonemes(_phonemes), syllables(_syllables) {};
+  const std::string stringify() const {
+    std::string ret;
+    ret += "\nword: " + word + "\n";
+    ret += "phonemes: " + std::string("\n");
+    for (auto &phoneme : phonemes) {
+      ret += phoneme + " ";
+    }
+    ret += "\n";
+    ret += "syllables: " + to_string(syllables);
+    ret += "\n";
+    return ret;
+  }
 };
 
-struct Builder {
-  static constexpr array<const char *, 13> vowels = {
-      "AA", "AE", "AH", "AO", "AW", "AY", "EH",
-      "ER", "EY", "IH", "IY", "UH", "UW"};
-  bool isVowel(const std::string &phoneme) {
+ostream &operator<<(ostream &lhs, const Token &token) {
+  lhs << token.stringify();
+  return lhs;
+}
+
+struct TokenMaker {
+  static constexpr array<const char *, 15> vowels = {
+      "AA", "AE", "AH", "AO", "AW", "AY", "EH", "ER",
+      "EY", "IH", "IY", "OY", "OW", "UH", "UW"};
+  static inline bool isVowel(const std::string &phoneme) {
     for (auto &vowel : vowels) {
       if (phoneme == vowel)
         return true;
     }
     return false;
   }
-
-  vector<string> getPhonemes(const std::string &line) {
-    return split<vector>(line);
+  static Token makeToken(const vector<string> &split_line) {
+    const string &word = split_line[0];
+    vector<std::string> phonemes;
+    for (int i = 0; i < split_line.size(); i++) {
+      phonemes.push_back(split_line[i]);
+    }
+    const unsigned int &syllable_count = TokenMaker::countSyllables(phonemes);
+    return Token(word, phonemes, syllable_count);
   }
-
-  unsigned int count_syllables(const vector<string> &phonemes) {
+  static unsigned int countSyllables(const vector<string> &phonemes) {
+    unsigned int count = 0;
     for (auto &phoneme : phonemes) {
-      unsigned int count = 0;
-      if (isVowel(phoneme)) {
+      if (TokenMaker::isVowel(phoneme)) {
         count++;
       }
-      return count;
     }
+    return count;
   }
 };
