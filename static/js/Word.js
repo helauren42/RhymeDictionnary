919ba3aa87700a591ab2b11c14e69a5fcfcdc51b
helauren <helauren42@gmail.com>
2025-03-26 22:08:29 +0100

    trimmed down word as backend phonemes parsing has changed

--- static/js/Word.js
+++ static/js/Word.js
@@ -5,34 +5,11 @@ export class Word {
     this.vowels = _vw;
     this.consonants = _conso;
     this.blocks = this.makeBlocks();
-    this.particles = this.makeParticles();
   }
   isVowel(phoneme) {
     if (this.vowels.includes(phoneme))
       return true;
     return false;
-  }
-  makeBlocks() {
-    let pos = 0;
-    let previousIsVowel = true;
-    let ret = []
-    for (let phoneme of this.phonemes) {
-      if (this.isVowel(phoneme) && (pos == 0 || previousIsVowel)) {
-        ret.push("");
-        ret.push(phoneme);
-      }
-    }
-    return ret;
-  }
-  makeParticles() {
-    let pos = 0;
-    let ret = [];
-    for (let block of this.blocks) {
-      if (this.isVowel(block)) {
-        ret.push(block);
-      }
-    }
-    return ret;
   }
 }
 
